eb4824f901b4f563b52199f37eb904f204a71730
Lahari-nagaraj <158993362+Lahari-nagaraj@users.noreply.github.com>
2025-02-14 14:55:37 +0530

    database

--- fund-a-child/server.js
+++ fund-a-child/server.js
@@ -13,10 +13,11 @@ app.use(bodyParser.json());
 
 // Database Connection
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: "postgres",
+  host: "localhost",
+  database: "FCI",
+  password: "hari@2013",
+  port: 5432,
 });
 
 // Add an entry
